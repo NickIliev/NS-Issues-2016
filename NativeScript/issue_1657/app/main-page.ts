919d47d4f8842a7ac7099c0714ff90977830f7ee
Nikolay Iliev <Nikolay.Iliev@telerik.com>
2016-10-10 11:08:41 +0300

    added issues

---
+++ NativeScript/issue_1657/app/main-page.ts
@@ -0,0 +1,22 @@
+import { EventData } from 'data/observable';
+import { Page } from 'ui/page';
+
+var frameModule = require("ui/frame"); 
+
+// Event handler for Page "navigatingTo" event attached in main-page.xml
+export function navigatingTo(args: EventData) {
+  // Get the event sender
+  let page = <Page>args.object;
+
+  frameModule.topmost().ios.controller.navigationBar.translucent = false;
+}
+
+export function onTap() {
+  frameModule.topmost().navigate({
+    moduleName: "sub-page",
+    // animated: true, // solution for the buggy transition when translucent set to false
+    // transition: {
+    //   name: "curl" // iOS only transition
+    // }
+  });
+}
