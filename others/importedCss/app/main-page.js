55542f27f93fa27638b9ae41bebdbbb7e86b641a
Nikolay Iliev <Nikolay.Iliev@telerik.com>
2016-10-11 17:03:49 +0300

    added issues

---
+++ others/importedCss/app/main-page.js
@@ -0,0 +1,8 @@
+"use strict";
+// Event handler for Page "navigatingTo" event attached in main-page.xml
+function navigatingTo(args) {
+    // Get the event sender
+    var page = args.object;
+}
+exports.navigatingTo = navigatingTo;
+//# sourceMappingURL=main-page.js.map
