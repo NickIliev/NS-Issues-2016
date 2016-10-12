77ff5475f4dfdccf303686bb4ebc521e108b6b9c
NickIliev <Nikolay.Iliev@telerik.com>
2016-10-12 13:08:46 +0300

    added new

---
+++ NativeScript/issue_2863/app/app.ts
@@ -0,0 +1,50 @@
+﻿import * as app from 'application';
+
+import {setActivityCallbacks, AndroidActivityCallbacks} from "ui/frame";
+
+@JavaProxy("com.tns.NativeScriptActivity")
+class Activity extends android.app.Activity {
+    private _callbacks: AndroidActivityCallbacks;
+
+    public onUserInteraction(): void {
+        console.log("onUserInteraction"); // OVERWRITTEN BEHAVIOUR
+    }
+
+    protected onCreate(savedInstanceState: android.os.Bundle): void {
+        if (!this._callbacks) {
+            setActivityCallbacks(this);
+        }
+
+        this._callbacks.onCreate(this, savedInstanceState, super.onCreate);
+    }
+
+    protected onSaveInstanceState(outState: android.os.Bundle): void {
+        this._callbacks.onSaveInstanceState(this, outState, super.onSaveInstanceState);
+    }
+
+    protected onStart(): void {
+        this._callbacks.onStart(this, super.onStart);
+    }
+
+    protected onStop(): void {
+        this._callbacks.onStop(this, super.onStop);
+    }
+
+    protected onDestroy(): void {
+        this._callbacks.onDestroy(this, super.onDestroy);
+    }
+
+    public onBackPressed(): void {
+        this._callbacks.onBackPressed(this, super.onBackPressed);
+    }
+
+    public onRequestPermissionsResult(requestCode: number, permissions: Array<String>, grantResults: Array<number>): void {
+        this._callbacks.onRequestPermissionsResult(this, requestCode, permissions, grantResults, undefined /*TODO: Enable if needed*/);
+    }
+
+    protected onActivityResult(requestCode: number, resultCode: number, data: android.content.Intent): void {
+        this._callbacks.onActivityResult(this, requestCode, resultCode, data, super.onActivityResult);
+    }
+}
+
+app.start({ moduleName: 'main-page' });
