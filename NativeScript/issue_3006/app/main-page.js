b5cd01760e61e08db1334d922f16df8472f0bc86
Nikolay Iliev <Nikolay.Iliev@telerik.com>
2016-11-02 15:54:00 +0200

    added

--- NativeScript/issue_3006/app/main-page.js
+++ NativeScript/issue_3006/app/main-page.js
@@ -1,14 +1,20 @@
 "use strict";
+var st;
 function navigatingTo(args) {
     var page = args.object;
+    st = page.getViewById("st");
 }
 exports.navigatingTo = navigatingTo;
-function onTouch(args) {
-    console.log("onTouch");
+function onStackTouch(args) {
+    console.log("onStackTouch");
 }
-exports.onTouch = onTouch;
+exports.onStackTouch = onStackTouch;
 function onTap(args) {
     console.log("onTap");
+    console.log(st.isUserInteractionEnabled);
+    // st.off("touch");
+    var view = args.object;
+    view.text += "ala";
 }
 exports.onTap = onTap;
 //# sourceMappingURL=main-page.js.map
--- NativeScript/issue_3006/app/main-page.ts
+++ NativeScript/issue_3006/app/main-page.ts
@@ -1,14 +1,22 @@
 import { EventData } from 'data/observable';
 import { Page } from 'ui/page';
+import { Label } from 'ui/label';
+let st;
 
 export function navigatingTo(args: EventData) {
   let page = <Page>args.object;
+  st = page.getViewById("st");
 }
 
-export function onTouch(args: EventData) {
-  console.log("onTouch");
-}
 
+export function onStackTouch(args: EventData) {
+  console.log("onStackTouch");
+}
 export function onTap(args: EventData) {
   console.log("onTap");
+
+  console.log(st.isUserInteractionEnabled)
+  // st.off("touch");
+  var view = <Label>args.object;
+  view.text += "ala";
 }
