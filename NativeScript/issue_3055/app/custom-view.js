3de99d775c6d31bb18c64b2171893f7eaab00c96
Nikolay Iliev <Nikolay.Iliev@telerik.com>
2016-11-11 08:52:50 +0200

    add

--- NativeScript/issue_3055/app/custom-view.js
+++ NativeScript/issue_3055/app/custom-view.js
@@ -1,13 +1,20 @@
 var StackLayout = require("ui/layouts/stack-layout").StackLayout;
 
 var CustomView = (function(_super) {
-  __extends(CustomView, _super);
-  function CustomView() {
-    _super.call(this);
-    _customView = this;
-  }
-  
-  return CustomView;
+    __extends(CustomView, _super);
+    function CustomView() {
+        _super.call(this);
+        _customView = this;
+
+        this.on("touch", function() {
+          console.log("CV touched from inside");
+        })
+
+        this.on("tap", function() {
+          console.log("CV tapped from inside");
+        })
+    }
+    return CustomView;
 })(StackLayout);
 
 exports.CustomView = CustomView;
