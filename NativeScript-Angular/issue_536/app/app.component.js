f208626c962b44fa0a0f8966936b288443edf77f
NickIliev <Nikolay.Iliev@telerik.com>
2016-11-15 16:57:41 +0200

    add

---
+++ NativeScript-Angular/issue_536/app/app.component.js
@@ -0,0 +1,17 @@
+"use strict";
+var core_1 = require("@angular/core");
+var AppComponent = (function () {
+    function AppComponent() {
+        this.myDate = new Date("Tue Nov 15 2016 21:02:49 GMT-0300 (UYT)");
+    }
+    AppComponent = __decorate([
+        core_1.Component({
+            selector: "my-app",
+            templateUrl: "app.component.html",
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppComponent);
+    return AppComponent;
+}());
+exports.AppComponent = AppComponent;
+//# sourceMappingURL=app.component.js.map
