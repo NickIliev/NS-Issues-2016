0f303f1aacd2d44a97ed18543053b4693bee8ac1
NickIliev <Nikolay.Iliev@telerik.com>
2016-11-18 09:17:27 +0200

    add

---
+++ NativeScript/issue_3109/app/main-page.ts
@@ -0,0 +1,43 @@
+/*
+In NativeScript, a file with the same name as an XML file is known as
+a code-behind file. The code-behind is a great place to place your view
+logic, and to set up your page’s data binding.
+*/
+
+import { EventData } from 'data/observable';
+import { Page } from 'ui/page';
+import { HelloWorldModel } from './main-view-model';
+
+import * as appSettings from "application-settings";
+
+// Event handler for Page "navigatingTo" event attached in main-page.xml
+export function navigatingTo(args: EventData) {
+    /*
+    This gets a reference this page’s <Page> UI component. You can
+    view the API reference of the Page to see what’s available at
+    https://docs.nativescript.org/api-reference/classes/_ui_page_.page.html
+    */
+    let page = <Page>args.object;
+    
+    var dNow = Date.now();
+
+    console.log(dNow); // => 1479390381732
+    appSettings.setNumber('dNow', dNow);
+    console.log(appSettings.getNumber('dNow')); // => 1479390396416
+
+    var dNumber = 1479390381732;
+    appSettings.setNumber('dNumber', dNumber);
+    console.log("dNumber:" + appSettings.getNumber('dNumber'));
+
+    /*
+    A page’s bindingContext is an object that should be used to perform
+    data binding between XML markup and TypeScript code. Properties
+    on the bindingContext can be accessed using the {{ }} syntax in XML.
+    In this example, the {{ message }} and {{ onTap }} bindings are resolved
+    against the object returned by createViewModel().
+
+    You can learn more about data binding in NativeScript at
+    https://docs.nativescript.org/core-concepts/data-binding.
+    */
+    page.bindingContext = new HelloWorldModel();
+}
